971ac19ec49f6486090068c0eb3d44c90e3617cb
Shirley Tang <shirleytang1996@gmail.com>
2021-08-16 11:35:52 -0400

    add nodemon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("Hello world");
+    res.send("Hello aa");
 })
 
 const port = process.env.PORT || 5000;
