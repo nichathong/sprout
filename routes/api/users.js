407746e1fabff27352fdce22bbe76bc7580c2ff0
Shirley Tang <shirleytang1996@gmail.com>
2021-08-16 14:12:59 -0400

    add bcypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express  = require("express");
 const router = express.Router();
 const User = require("../../models/User")
+const bcrypt = require("bcryptjs")
 
 router.get("/test",(req,res)=>{
     res.json({msg: "this is user routes"})
@@ -17,7 +18,15 @@ router.post("/register",(req,res) =>{
           password: req.body.password
         })
 
-        newUser.save().then(user => res.send(user)).catch(err => res.send(err));
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                if (err) throw err;
+                newUser.password =hash;
+                newUser.save().then(user => res.json(user)).catch(err => console.log(err));
+            })
+        })
+
+      
     }
     })
 })
