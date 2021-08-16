4f963af7f35e72258c3710fa5cfd8d0815c52608
Shirley Tang <shirleytang1996@gmail.com>
2021-08-16 14:04:01 -0400

    work on register

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI
 const users = require("./routes/api/users")
 const User  = require("./models/User")
+const bodyParser = require('body-parser');
 
 mongoose
 .connect(db, {useNewUrlParser: true})
 .then(()=>console.log('connet to mongoDB'))
 .catch(err=>console.log(err))
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/",(req,res)=>{
-    const user =new User({
-        email: 'demo1@gmail.com',
-        password:'123456'
-    });
-    user.save()
+    // const user =new User({
+    //     email: 'demo1@gmail.com',
+    //     password:'123456'
+    // });
+    // user.save()
     res.send("Hello aa");
 })
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,25 @@
 const express  = require("express");
 const router = express.Router();
+const User = require("../../models/User")
 
 router.get("/test",(req,res)=>{
     res.json({msg: "this is user routes"})
+})
+
+router.post("/register",(req,res) =>{
+    User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({email: "A user has already registered with this address"})
+      } else {
+        const newUser = new User({
+          email: req.body.email,
+          password: req.body.password
+        })
+
+        newUser.save().then(user => res.send(user)).catch(err => res.send(err));
+    }
+    })
 })
 
 module.exports = router;
