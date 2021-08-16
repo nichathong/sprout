4b0c588fcfd99d997c80a5592577ea564eed0e7f
Shirley Tang <shirleytang1996@gmail.com>
2021-08-16 13:16:58 -0400

    link to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI
+
+mongoose
+.connect(db, {useNewUrlParser: true})
+.then(()=>console.log('connet to mongoDB'))
+.catch(err=>console.log(err))
 
 app.get("/",(req,res)=>{
     res.send("Hello aa");
