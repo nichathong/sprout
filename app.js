44b410119e7fdcad3ec0b00374352e098245b3b5
Shirley Tang <shirleytang1996@gmail.com>
2021-08-16 16:31:41 -0400

    update passport

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const db = require("./config/keys").mongoURI
 const users = require("./routes/api/users")
 const User  = require("./models/User")
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
 .connect(db, {useNewUrlParser: true})
 .then(()=>console.log('connet to mongoDB'))
 .catch(err=>console.log(err))
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     // const user =new User({
@@ -22,6 +20,15 @@ app.get("/",(req,res)=>{
     // user.save()
     res.send("Hello aa");
 })
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 
 app.use("/api/users",users);
 
