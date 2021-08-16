c295151367cde21bb563e7a4f380cb89fe69a819
patrickc <patrick.evan.campbell@gmail.com>
2021-08-16 17:55:14 -0600

    add session form

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,7 +5,7 @@ import { Switch } from 'react-router-dom';
 
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
-// import SignupFormContainer from './session/signup_form_container';
+import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
   <div>
@@ -13,7 +13,7 @@ const App = () => (
     <Switch>
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginFormContainer} />
-        {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </Switch>
   </div>
 );
--- frontend/src/components/session/login_form_container.js
+++ frontend/src/components/session/login_form_container.js
@@ -1,20 +1,14 @@
-import { connect } from 'react-redux';
-import { login } from '../../actions/session_actions';
-import LoginForm from './login_form';
+import { connect } from "react-redux";
+import SessionForm from "./session_form";
+// import { login } from "../../actions/session_actions";
 
-const mapStateToProps = (state) => {
-  return {
-    errors: state.errors.session
-  };
-};
+const mstp = state => ({
+    // errors: state.errors.session,
+    formType: "Login"
+});
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    login: user => dispatch(login(user))
-  }
-}
+const mdtp = dispatch => ({
+    // login: user => dispatch(login(user))
+});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(LoginForm);
+export default connect(mstp, mdtp)(SessionForm);
