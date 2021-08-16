5128eacbf306fe5c5d03203ca1c7441249a7dd92
nichathong <nicha.fn@gmail.com>
2021-08-16 15:59:27 -0700

    created front end and started on user component

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Sprout</h1>
+        <footer>
+          Copyright &copy; 2021 Sprout team
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
