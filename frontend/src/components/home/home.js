dfe8244846918fb3dbc7bb0f35f13321f6fc26b0
Annie Yang <annieqiyang@gmail.com>
2021-08-17 11:34:10 -0400

    added home container

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+
+class Home extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (<div></div>)
+    }
+}
+
+export default Home;
