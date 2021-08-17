63a458cfb8fa2318a9917b7e7686e7ebeb780e4e
Shirley Tang <shirleytang1996@gmail.com>
2021-08-16 20:11:41 -0400

    work on plants

--- routes/api/plants.js
+++ routes/api/plants.js
@@ -1,28 +1,28 @@
 const express  = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken")
-const passport  = require("passport")
+const jwt = require("jsonwebtoken");
+const passport  = require("passport");
 const Plant = require("../../models/Plant")
 
-router.get("/test",(req,res)=>{
+router.get("/tests",(req,res)=>{
     res.json({msg: "this is plant routes"})
 })
 
-router.post("/new",passport.authenticate('jwt', { session: false }),(req,res)=>{
-    const newPlant = new Plant({
-        author: req.user.id,
-        name: req.body.name,
-        // tags: req.body.tags,
-        // waterLevel: req.body.waterLevel,
-        // light: req.body.light,
-        // temperature: req.body.temperature,
-        // level: req.body.level,
-        // waterFrequency: req.body.waterFrequency,
-        // photoUrls: req.body.photoUrls
+// router.post("/new",passport.authenticate('jwt', { session: false }),(req,res)=>{
+//     const newPlant = new Plant({
+//         author: req.user.id,
+//         name: req.body.name,
+//         tags: req.body.tags,
+//         waterLevel: req.body.waterLevel,
+//         light: req.body.light,
+//         temperature: req.body.temperature,
+//         level: req.body.level,
+//         waterFrequency: req.body.waterFrequency,
+//         photoUrls: req.body.photoUrls
 
-    });
-    newPlant.save().then(plant => res.json(plant))
-})
+//     });
+//     newPlant.save().then(plant => res.json(plant))
+// })
 
 
-module.exports = router
+module.exports = router;
