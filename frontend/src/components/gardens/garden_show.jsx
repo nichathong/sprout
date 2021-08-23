a58605cf61f23f4d70dccefdbbfa8ea848a528b7
patrickc <patrick.evan.campbell@gmail.com>
2021-08-23 10:50:53 -0600

    finish garden index

--- frontend/src/components/gardens/garden_show.jsx
+++ frontend/src/components/gardens/garden_show.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./garden_index.css";
+import { Link } from 'react-router-dom';
 
 
 class GardenShow extends React.Component {
@@ -9,10 +10,25 @@ class GardenShow extends React.Component {
     }
 
 
+    renderPlantDetail(gardenPlant) {
+        const { plants } = this.props
+        for (let i = 0; i < plants.length; i++) {
+            if (plants[i]._id === gardenPlant.plant) {
+                return (
+                    <Link to={`/plants/${gardenPlant.plant}`}><img className="garden-plant-image" src={plants[i].photoUrls[0]} /></Link>
+                )
+            }
+        }
+    }
+
+
     render() {
         const { user, plants, gardenPlants } = this.props;
 
         // console.log(`${user.firstname} ------- ${gardenPlants.length}`);
+
+        var today = new Date();
+        var days = 0;
 
         return (
             gardenPlants.length > 0 ?
@@ -21,7 +37,17 @@ class GardenShow extends React.Component {
 
                     <h3 className="user-garden-header">{user.firstname}'s Garden</h3>
 
-                    <img className="user-garden-dirt" src="dirt.jpeg" />
+                    <div className="user-garden-dirt">
+                        {gardenPlants.map((gardenPlant, idx) => (
+                            <li className="individual-sprout-2" key={idx}>
+                                <div className="plantName3"> {gardenPlant.nickname} </div>
+                                Days alive: {(days = Math.round(Math.ceil(new Date() - new Date(gardenPlant.date.split("T")[0])) / (1000 * 60 * 60 * 24)))} <br />
+                                {days < 3 ? <img className="sprout-image" src="https://sprout-app.s3.us-east-2.amazonaws.com/plant-10.png" /> : (days < 6 ? <img className="sprout-image" src="https://sprout-app.s3.us-east-2.amazonaws.com/plant-20.png" /> : (days < 9 ? <img className="sprout-image" src="https://sprout-app.s3.us-east-2.amazonaws.com/plant-3.png" /> : (days < 12 ? <img className="sprout-image" src="https://sprout-app.s3.us-east-2.amazonaws.com/plant-4.png" /> : <img className="sprout-image" src="https://sprout-app.s3.us-east-2.amazonaws.com/plant-5.png" />)))}
+                                <br />
+                                {this.renderPlantDetail(gardenPlant)}
+                            </li>
+                        ))}
+                    </div>
                 </li>
                 : null
         );
